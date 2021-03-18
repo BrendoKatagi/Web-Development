bb95a440e577b6141f4434f6c3077a8611f883a2
Brendo Katagi <brendo.katagi@gmail.com>
2021-03-18 09:38:12 -0300

    Feat: Add post function to get the data from front-end

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo&appid=c85446ae4c3c824ead6aded5f3bbf12c&units=metric";
-    https.get(url, function(response){
-        console.log(response.statusCode);
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res){
+
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+
+    const query = req.body.cityName;
+    const apiKey = "c85446ae4c3c824ead6aded5f3bbf12c";
+    const unit = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    https.get(url, function(response){
+        
         response.on("data", function(data){
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
@@ -16,13 +27,13 @@ app.get("/", function(req, res){
             const imgUrl = "http://openweathermap.org/img/wn/" + icon + ".png" 
 
             res.write("<p>The weather is currently: " + weatherDescription + " </p>");
-            res.write("<h1>The temperature in Sao Paulo is " + temp + " degrees Celsius</h1>");
+            res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celsius</h1>");
             res.write("<img src='"+ imgUrl +"'>");
             res.send();
         })
     })
+});
 
-})
 
 app.listen(3000, function(){
     console.log("Server is running on port 3000")
