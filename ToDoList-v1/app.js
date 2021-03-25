4cddc066599de8a6cb1aaae1a965d6cb64e51ff5
Brendo Katagi <brendo.katagi@gmail.com>
2021-03-25 11:00:20 -0300

    Finish application

--- ToDoList-v1/app.js
+++ ToDoList-v1/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const getDate = require("./date");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
 let items = ["Buy Food","Cook Food","Eat Food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -11,26 +14,36 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    
-    let today = new Date();
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
-
-    res.render('list', {kindOfDay: day, newListItems: items});
-    //res.sendFile(__dirname + "/index.html");
+    let day = date.getDate();
+    res.render('list', {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
+
     let item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+
     items.push(item);
 
     res.redirect("/");
+});
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+});
+
+app.post("/work", function(req, res){
+   let item = req.body.newItem;
+   workItems.push(item);
+   res.redirect("/work");
 });
 
 app.listen(3000, function(){
