c78b140e7ac7fc2e1b97829e0f32e54db9ebe220
Brendo Katagi <brendo.katagi@gmail.com>
2021-10-02 21:11:27 -0300

    feat: starting files for Keeper App

---
+++ React/Keeper-app/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(){
+    return (
+        <div className = "note">
+            <h1>Teste</h1>
+            <p>testes 2</p>
+        </div>
+    );
+}
+
+export default Note;
