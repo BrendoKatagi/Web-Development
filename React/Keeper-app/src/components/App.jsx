417b5b85869719af416524a278971ffdb7169d88
Brendo Katagi <brendo.katagi@gmail.com>
2021-10-03 01:50:05 -0300

    feat: created button component do insert new notes

--- React/Keeper-app/src/components/App.jsx
+++ React/Keeper-app/src/components/App.jsx
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import notes from "../notes";
 import setNote from "../scripts/setNotes";
+import Button from "./Button";
 
 function App() {
     return (
         <div>
             <Header />
             {notes.map(setNote)}
+            <Button />
             <Footer />
         </div>
     );
