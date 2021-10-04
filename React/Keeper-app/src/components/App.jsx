53c1cbd8fc4f20b41333264807af6669a8b91627
Brendo Katagi <brendo.katagi@gmail.com>
2021-10-04 00:10:27 -0300

    feat: create functions to add new notes and delete specific notes

--- React/Keeper-app/src/components/App.jsx
+++ React/Keeper-app/src/components/App.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import notes from "../notes";
-import setNote from "../scripts/setNotes";
-import Button from "./Button";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
 
 function App() {
+    const [notes, setNotes] = useState([]);
+
+    function defineNotes(newNote){
+        setNotes((prevNotes) => {
+            return([
+                ...prevNotes,
+                newNote
+            ]);
+        })
+    }
+
+    function deleteNote(id){
+        setNotes(prevNotes => {
+            return prevNotes.filter((note, index) => {
+                return(index !== id);
+            })
+        })
+    }
+
     return (
         <div>
-            <Header />
-            {notes.map(setNote)}
-            <Button />
-            <Footer />
+        <Header />
+        <CreateArea defineNotes = {defineNotes}/>
+        {notes.map((note, index) => {
+            return <Note key={index} id={index} title={note.title} content={note.content} onDelete={deleteNote}/>
+        })}
+        <Footer />
         </div>
     );
 }
