d067ed668cb4da3e7d24ceab78f345e2d29516ff
Brendo Katagi <brendo.katagi@gmail.com>
2021-10-03 21:56:57 -0300

    feat: created function to pass the id when an item got clicked

--- React/ToDoList/src/components/ListItem.jsx
+++ React/ToDoList/src/components/ListItem.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function ListItem(props) {
-  return <li>{props.value}</li>;
+  return (
+    <div
+      onClick={() => {
+        props.onChecked(props.id);
+      }}
+    >
+      <li>{props.value}</li>;
+    </div>
+  );
 }
 
 export default ListItem;
